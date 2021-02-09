38998d00221566d919f4c08364768dd5cf980941
prerana19 <67142244+prerana19@users.noreply.github.com>
2021-02-09 11:21:28 +0530

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -26,6 +26,8 @@ Mat readImg(string& fileName, string type) {
     return inputImage;
 }
 
+
+
 // Function reads the image and creates its corresponding histogram.
 
 void imgToHist(Mat image, float histogram[], int size) {
@@ -42,6 +44,8 @@ void imgToHist(Mat image, float histogram[], int size) {
 
 }
 
+
+
 //Calculating the cumulative frequency distribution of the given histogram
 
 void CDF(float histogram[], float tranFunc[]) {
@@ -51,6 +55,8 @@ void CDF(float histogram[], float tranFunc[]) {
         tranFunc[i] = histogram[i] + tranFunc[i - 1];
 
 }
+
+
 
 // This function is used to map the histogram to the intensity values that will be displayed on the image
 
@@ -62,6 +68,8 @@ void intensityMapping(float tranFunc[], float histogram[]) {
         histogram[i] = (((tranFunc[i] - tranFuncMin) / (1 - tranFuncMin)) * (LEVEL - 1) + 0.5);
 
 }
+
+
 
 //Function to match histogram of the input image to the target image
 
@@ -76,6 +84,8 @@ void histogramMatching(float inputTranFunc[], float targetTranFunc[], float hist
     }
     return;
 }
+
+
 
 //Function to display histogram of an image and to write the histogram in the output file
 
@@ -110,7 +120,7 @@ void showHist(Mat& image, string fileName) {
 
 int main() {
 
-    int option;
+    int task;
     string name;
     cout << "ASSIGNMENT 2: HISTOGRAM EQUALISATION AND MATCHING" << endl;
     char exit_key;
@@ -118,11 +128,17 @@ int main() {
         cout << " Enter task number to execute " << endl;
         cout << "   1. Histogram Equalization" << endl;
         cout << "   2. Histogram Matching" << endl;
-        cin >> option;
-        if (option == 1) {
+        cin >> task;
+
+
+
+        //Histogram Equalization function
+        if (task == 1) {
             cout << "\nHistogram Equalization" << endl;
             string fileName;
 
+
+            //Reading the image input
             Mat inputImage = readImg(fileName, "Input");
             if (inputImage.empty()) {
                 cerr << "Error in loading image" << endl;
@@ -130,31 +146,44 @@ int main() {
                 return -1;
             }
 
+
+
+            //Executed if the image is grayscale
             if (inputImage.channels() == 1) {
 
                 int size = inputImage.rows * inputImage.cols;
 
+
+                //Converting image to histogram
                 float histogram[LEVEL];
                 imgToHist(inputImage, histogram, size);
 
+
+                //Calculating the CDF from histogram
                 float tranFunc[LEVEL];
                 CDF(histogram, tranFunc);
 
+
+                //Mapping the intensities across the intensity levels
                 float outHistogram[LEVEL];
                 intensityMapping(tranFunc, outHistogram);
 
+
+                //creating output image using the output histogram
                 Mat outputImage = inputImage.clone();
                 for (int y = 0; y < inputImage.rows; y++)
                     for (int x = 0; x < inputImage.cols; x++)
                         outputImage.at<uchar>(y, x) = saturate_cast<uchar>(saturate_cast<int>(outHistogram[inputImage.at<uchar>(y, x)]));
 
+
+
+                //Naming and displaying images and histogram windows
                 namedWindow("Original Image");
                 imshow("Original Image", inputImage);
                 showHist(inputImage, " Original Histogram");
 
                 namedWindow("Histogram Equilized Image");
                 imshow("Histogram Equilized Image", outputImage);
-                //imwrite("EQ_img.bmp",outputImage);
                 showHist(outputImage, " Equalized Histogram");
 
                 waitKey();
@@ -164,11 +193,17 @@ int main() {
             }
 
         }
-        else if (option == 2) {
+
+
+
+        //Histogram Matching
+        else if (task == 2) {
             cout << "\nHistogram Matching" << endl;
 
             string inputFileName, targetFileName;
 
+
+            //Reading the input and target images
             Mat inputImage = readImg(inputFileName, "Input");
             if (inputImage.empty()) {
                 cerr << "Error in loading image" << endl;
@@ -187,9 +222,14 @@ int main() {
             cout << "inputFileName: " << inputFileName << endl;
             cout << "targetFileName: " << targetFileName << endl;
 
+
+
+            //Executed if the images are grayscale
             if (inputImage.channels() == 1 && targetImage.channels() == 1) {
                 int inputSize = inputImage.rows * inputImage.cols;
 
+
+                //Converting images to histograms and calculating the CDF from histograms
                 float inputHistogram[LEVEL];
                 imgToHist(inputImage, inputHistogram, inputSize);
 
@@ -203,15 +243,24 @@ int main() {
                 float targetTranFunc[LEVEL];
                 CDF(targetHistogram, targetTranFunc);
 
+
+                //Matching the input CDF with the target CDF
                 float outHistogram[LEVEL];
                 histogramMatching(inputTranFunc, targetTranFunc, outHistogram);
 
+
+
+                //creating output image using the output histogram
                 Mat outputImage = inputImage.clone();
 
                 for (int y = 0; y < inputImage.rows; y++)
                     for (int x = 0; x < inputImage.cols; x++)
                         outputImage.at<uchar>(y, x) = (int)(outHistogram[inputImage.at<uchar>(y, x)]);
 
+
+
+
+                //Naming and displaying images and histogram windows
                 namedWindow("Original Image");
                 imshow("Original Image", inputImage);
                 showHist(inputImage, " Original Histogram");
