87f1fda18ea8edb700e545301142db2fd2390433
prerana19 <67142244+prerana19@users.noreply.github.com>
2021-02-10 15:53:53 +0530

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -15,6 +15,7 @@ using namespace cv;
 
 // Function reads the image file and returns a Mat type image container
 
+
 Mat readImg(string& fileName, string type) {
     cout << endl << "Enter" << type << " Image:" << endl;
     cout << "Example <cameraman.bmp> <sample1.tiff> <lena.jpg>" << endl;
@@ -44,6 +45,26 @@ void imgToHist(Mat image, float histogram[], int size) {
 
 }
 
+void intensityToHistogram(float** I, float histogram[], int rows, int cols) {
+
+    for (int i = 0; i < LEVEL; i++) {
+        histogram[i] = 0;
+    }
+
+    for (int y = 0; y < rows; y++) {
+        for (int x = 0; x < cols; x++) {
+            histogram[(int)I[y][x]]++;
+        }
+    }
+
+    for (int i = 0; i < LEVEL; i++) {
+        histogram[i] = histogram[i] / (rows * cols);
+    }
+
+    return;
+
+}
+
 
 
 //Calculating the cumulative frequency distribution of the given histogram
@@ -69,6 +90,114 @@ void intensityMapping(float tranFunc[], float histogram[]) {
 
 }
 
+//Function to convert an Red Green Blue(RGB) space to Hue Saturation Intensity(HSI) space
+void convert_RGB_To_HSI(Mat inputImage, Mat inputImageHSI, float** H, float** S, float** I) {
+    double r, g, b, h, s, in;
+    //formula based conversion
+    for (int i = 0; i < inputImage.rows; i++)
+    {
+        for (int j = 0; j < inputImage.cols; j++)
+        {
+
+            b = inputImage.at<Vec3b>(i, j)[0];
+            g = inputImage.at<Vec3b>(i, j)[1];
+            r = inputImage.at<Vec3b>(i, j)[2];
+
+
+
+            float min_val = 0.0;
+            min_val = min(r, min(b, g));
+            s = 1 - 3 * (min_val / (b + g + r));
+
+            in = (b + g + r) / 3; // TO SEE
+
+            if (s < 0.00001)
+            {
+                s = 0.0;
+            }
+            else if (s > 0.99999) {
+                s = 1.0;
+            }
+
+            if (s != 0.0)
+            {
+                h = 0.5 * ((r - g) + (r - b)) / sqrt(((r - g) * (r - g)) + ((r - b) * (g - b)));
+                h = acos(h);
+
+                if (b <= g)
+                {
+                    h = h;
+                }
+                else {
+                    h = ((360 * 3.14159265) / 180.0) - h;
+                }
+            }
+            else {
+                h = 0.0;
+            }
+
+            inputImageHSI.at<Vec3b>(i, j)[0] = H[i][j] = (h * 180) / 3.14159265;
+            inputImageHSI.at<Vec3b>(i, j)[1] = S[i][j] = s * 100;
+            inputImageHSI.at<Vec3b>(i, j)[2] = I[i][j] = in;
+
+        }
+    }
+    return;
+}
+
+//Function to convert an  Hue Saturation Intensity(HSI) space to Red Green Blue(RGB) space
+void convert_HSI_To_RGB(Mat outputImage, Mat inputImageHSI, float** H, float** S, float** I) {
+    float r, g, b, h, s, in;
+    //formula based conversion
+    for (int i = 0; i < inputImageHSI.rows; i++) {
+        for (int j = 0; j < inputImageHSI.cols; j++) {
+
+            h = H[i][j];
+            s = S[i][j] / 100;
+            in = I[i][j];
+
+            if (h >= 0.0 && h < 120.0) {
+                b = in * (1 - s);
+                r = in * (1 + (s * cos(h * 3.14159265 / 180.0) / cos((60 - h) * 3.14159265 / 180.0)));
+                g = 3 * in - (r + b);
+            }
+            else if (h >= 120.0 && h < 240.0) {
+                h = h - 120;
+                r = in * (1 - s);
+                g = in * (1 + (s * cos(h * 3.14159265 / 180.0) / cos((60 - h) * 3.14159265 / 180.0)));
+                b = 3 * in - (r + g);
+            }
+            else {
+                h = h - 240;
+                g = in * (1 - s);
+                b = in * (1 + (s * cos(h * 3.14159265 / 180.0) / cos((60 - h) * 3.14159265 / 180.0)));
+                r = 3 * in - (g + b);
+            }
+
+            if (b < INTENSITY_MIN)
+                b = INTENSITY_MIN;
+            if (b > INTENSITY_MAX)
+                b = INTENSITY_MAX;
+
+            if (g < INTENSITY_MIN)
+                g = INTENSITY_MIN;
+            if (g > INTENSITY_MAX)
+                g = INTENSITY_MAX;
+
+            if (r < INTENSITY_MIN)
+                r = INTENSITY_MIN;
+            if (r > INTENSITY_MAX)
+                r = INTENSITY_MAX;
+
+            outputImage.at<Vec3b>(i, j)[0] = round(b);
+            outputImage.at<Vec3b>(i, j)[1] = round(g);
+            outputImage.at<Vec3b>(i, j)[2] = round(r);
+
+        }
+    }
+    return;
+}
+
 
 
 //Function to match histogram of the input image to the target image
@@ -86,34 +215,57 @@ void histogramMatching(float inputTranFunc[], float targetTranFunc[], float hist
 }
 
 
+//Function to display histogram of an image
 
-//Function to display histogram of an image and to write the histogram in the output file
+void showHistogram(Mat& image, string fileName) {
+    int bins = 256;             // number of bins
+    int nc = image.channels();    // number of channels
+    vector<Mat> histogram(nc);       // array for storing the histograms
+    vector<Mat> canvas(nc);     // images for displaying the histogram
+    int hmax[3] = { 0,0,0 };      // peak value for each histogram
+    string name = "./output/histo_";     //for saving
 
-void showHist(Mat& image, string fileName) {
+    // The rest of the code will be placed here
+    for (int i = 0; i < histogram.size(); i++)
+        histogram[i] = Mat::zeros(1, bins, CV_32SC1);
 
-    int bins = 256;    // number of bins
-    Mat histogram;     // for storing the histogram
-    Mat canvas;		   // for displaying the histogram
-    int hmax = 0;      // peak value for each histogram
-
-    histogram = Mat::zeros(1, bins, CV_32SC1);
-
-    for (int i = 0; i < image.rows; i++)
+    //histogram calculation
+    for (int i = 0; i < image.rows; i++) {
         for (int j = 0; j < image.cols; j++) {
-            uchar val = image.at<uchar>(i, j);
-            histogram.at<int>(val) += 1;
+            for (int k = 0; k < nc; k++) {
+                uchar val = nc == 1 ? image.at<uchar>(i, j) : image.at<Vec3b>(i, j)[k];
+                histogram[k].at<int>(val) += 1;
+            }
+        }
+    }
+
+    //calculation maximum value of each histogram
+    for (int i = 0; i < nc; i++) {
+        for (int j = 0; j < bins - 1; j++)
+            hmax[i] = histogram[i].at<int>(j) > hmax[i] ? histogram[i].at<int>(j) : hmax[i];
+    }
+
+    //names
+    const char* wname[3] = { "Blue", "Green", "Red" };
+    Scalar colors[3] = { Scalar(255,0,0), Scalar(0,255,0), Scalar(0,0,255) };
+
+    //drawing the lines
+    for (int i = 0; i < nc; i++) {
+        canvas[i] = Mat::ones(125, bins, CV_8UC3);
+
+        for (int j = 0, rows = canvas[i].rows; j < bins - 1; j++) {
+            line(
+                canvas[i],
+                Point(j, rows),
+                Point(j, rows - (histogram[i].at<int>(j) * rows / hmax[i])),
+                nc == 1 ? Scalar(255, 255, 255) : colors[i],
+                1, 8, 0
+            );
         }
 
-    for (int j = 0; j < bins - 1; j++)
-        hmax = histogram.at<int>(j) > hmax ? histogram.at<int>(j) : hmax;
+        imshow(nc == 1 ? fileName : wname[i] + fileName, canvas[i]);
 
-    canvas = Mat::ones(125, bins, CV_8UC3);
-
-    for (int j = 0, rows = canvas.rows; j < bins - 1; j++)
-        line(canvas, Point(j, rows), Point(j, rows - (histogram.at<int>(j) * rows / hmax)), Scalar(255, 255, 255), 1, 8, 0);
-
-    imshow(fileName, canvas);
-
+    }
 }
 
 
@@ -180,18 +332,66 @@ int main() {
                 //Naming and displaying images and histogram windows
                 namedWindow("Original Image");
                 imshow("Original Image", inputImage);
-                showHist(inputImage, " Original Histogram");
+                showHistogram(inputImage, " Original Histogram");
 
                 namedWindow("Histogram Equilized Image");
                 imshow("Histogram Equilized Image", outputImage);
-                showHist(outputImage, " Equalized Histogram");
+                showHistogram(outputImage, " Equalized Histogram");
 
                 waitKey();
             }
-            else {
-                cout << " Not valid image type " << endl;
-            }
 
+            if (inputImage.channels() == 3) {
+
+                Mat inputImageHSI(inputImage.rows, inputImage.cols, inputImage.type());
+                //convert to HSI format
+                float** H = new float* [inputImage.rows];
+                float** S = new float* [inputImage.rows];
+                float** I = new float* [inputImage.rows];
+                for (int i = 0; i < inputImage.rows; i++) {
+                    H[i] = new float[inputImage.cols];
+                    S[i] = new float[inputImage.cols];
+                    I[i] = new float[inputImage.cols];
+                }
+
+                convert_RGB_To_HSI(inputImage, inputImageHSI, H, S, I);
+
+                float histogram[LEVEL];
+
+                intensityToHistogram(I, histogram, inputImage.rows, inputImage.cols);
+
+                float tranFunc[LEVEL];
+                CDF(histogram, tranFunc);
+
+                //outHistogram is the mapping
+                float outHistogram[LEVEL];
+                intensityMapping(tranFunc, outHistogram);
+
+
+                float** outI = new float* [inputImage.rows];
+                for (int i = 0; i < inputImage.rows; i++) {
+                    outI[i] = new float[inputImage.cols];
+                }
+
+                for (int i = 0; i < inputImage.rows; i++) {
+                    for (int j = 0; j < inputImage.cols; j++) {
+                        outI[i][j] = (int)outHistogram[(int)I[i][j]];
+                    }
+                }
+
+                Mat outputImage(inputImage.rows, inputImage.cols, inputImage.type());
+                convert_HSI_To_RGB(outputImage, inputImageHSI, H, S, outI);
+
+                namedWindow("Original Image", WINDOW_AUTOSIZE);
+                imshow("Original Image", inputImage);
+                showHistogram(inputImage, "Original Histogram");
+
+                namedWindow("Histogram Equalized Color Image", WINDOW_AUTOSIZE);
+                imshow("Histogram Equalized Color Image", outputImage);
+                showHistogram(outputImage, "Equalized Color Histogram");
+
+                waitKey();
+            }
         }
 
 
@@ -263,15 +463,15 @@ int main() {
                 //Naming and displaying images and histogram windows
                 namedWindow("Original Image");
                 imshow("Original Image", inputImage);
-                showHist(inputImage, " Original Histogram");
+                showHistogram(inputImage, " Original Histogram");
 
                 namedWindow("Target Image");
                 imshow("Target Image", targetImage);
-                showHist(targetImage, " Target Histogram");
+                showHistogram(targetImage, " Target Histogram");
 
                 namedWindow("Histogram Matched Image");
                 imshow("Histogram Matched Image", outputImage);
-                showHist(outputImage, " Matched Histogram");
+                showHistogram(outputImage, " Matched Histogram");
 
                 waitKey();
 
